1c00d2b7d8198f751ff26f4800735070c0ba24e6
Ejaz Merchant <ejaz.merchant@icloud.com>
2017-11-30 10:50:26 -0800

    New project and added MapKit

---
+++ mARks/MapViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MapViewController.swift
+//  mARks
+//
+//  Created by Ejaz Merchant on 11/29/17.
+//  Copyright © 2017 Ejaz Merchant. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+
+    var locationManager: CLLocationManager!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.locationManager = CLLocationManager()
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.startUpdatingLocation()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
