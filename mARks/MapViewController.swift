951133c9320aa020f2df5fb48353fd2ef3b563ac
Ejaz Merchant <ejaz.merchant@icloud.com>
2017-11-30 16:38:02 -0800

    Stop tracking user location after initial viewDidLoad

--- mARks/MapViewController.swift
+++ mARks/MapViewController.swift
@@ -18,16 +18,12 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = locations[0]
-        
         let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
-        
         let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
-        
         let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
-        
         mapView.setRegion(region, animated: true)
-        
         self.mapView.showsUserLocation = true
+        locationManager.stopUpdatingLocation()
     }
     
     override func viewDidLoad() {
